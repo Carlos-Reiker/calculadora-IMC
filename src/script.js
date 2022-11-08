048dcb37276c16dcc7ad75663b050600796595e4
Carlos Reiker <carlos.reiker@hotmail.com>
2022-11-08 20:48:29 -0300

    08/11/22

--- src/script.js
+++ src/script.js
@@ -1,2 +1,8 @@
+var peso = document.querySelector('#seuPeso');
+var altura = document.querySelector('#suaAltura');
+var resultado = document.querySelector('#resultado'); 
 
-
+function calcIMC(){
+    var imc = (peso.value / (altura.value * altura.value)).toFixed(2);
+    resultado.innerHTML = imc;
+}
